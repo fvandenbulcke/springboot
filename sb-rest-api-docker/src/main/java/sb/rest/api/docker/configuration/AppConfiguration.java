65bb504b97b48286c54b9ed2320e32c25755edc5
Florian vandenbulcke <florian.vandenbulcke@ext.adeo.com>
2018-08-09 15:09:16 +0200

    feat(sb-docker) + unit / integration tests

--- sb-rest-api-docker/src/main/java/sb/rest/api/docker/configuration/AppConfiguration.java
+++ sb-rest-api-docker/src/main/java/sb/rest/api/docker/configuration/AppConfiguration.java
@@ -1,9 +1,13 @@
 package sb.rest.api.docker.configuration;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import sb.rest.api.docker.MainClassApplication;
+
 @Configuration
+@ComponentScan(basePackageClasses=MainClassApplication.class)
 @EnableConfigurationProperties(value = {AppProperties.class})
 public class AppConfiguration {
 	
