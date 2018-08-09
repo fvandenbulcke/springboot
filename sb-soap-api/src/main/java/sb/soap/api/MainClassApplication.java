427979e677672daba891ab3e8dd14f18855108ec
Florian vandenbulcke <florian.vandenbulcke@ext.adeo.com>
2018-08-09 17:47:48 +0200

    feat(sb-soap):init projet fonctionnel

---
+++ sb-soap-api/src/main/java/sb/soap/api/MainClassApplication.java
@@ -0,0 +1,13 @@
+package sb.soap.api;
+
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainClassApplication {
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(MainClassApplication.class, args);
+	}
+}
