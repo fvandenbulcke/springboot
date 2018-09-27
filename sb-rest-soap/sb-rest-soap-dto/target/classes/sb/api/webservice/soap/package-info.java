2dfa90e1b86522f38fce3f1c70d71b2b5672f01b
Florian VANDENBULCKE <fvandenbulcke@sii.fr>
2018-09-27 14:07:59 +0200

    feat(dto):delete target folder

--- sb-rest-soap/sb-rest-soap-dto/target/classes/sb/api/webservice/soap/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.3.0 
-// Voir <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
-// Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2018.09.27 à 12:08:38 PM CEST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservice.api.sb/soap", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package sb.api.webservice.soap;
